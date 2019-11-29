ce1c7358a42292396e4e053ce48a33a904c3cd3d
Will Koste <willkoste@gmail.com>
2019-11-29 10:44:12 -0500

    Security Packages

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 // const logger = require('./middleware/logger');
@@ -39,6 +45,29 @@ if(process.env.NODE_ENV === 'development'){
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize Data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent xss attacks
+app.use(xssClean());
+
+// Set rate limit 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
